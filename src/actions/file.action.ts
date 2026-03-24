93a2194ab5bd7c97d6bee2f4ee68079f59c68d44
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-03-24 13:05:50 +0600

    feat: image upload service and action added

---
+++ src/actions/file.action.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { fileService } from "@/service/file.server.service";
+
+export async function uploadImagesAction(formData: FormData) {
+    try {
+        const res = await fileService.uploadImages(formData);
+
+        if (!res?.ok) {
+            return {
+                ok: false,
+                message: res?.message || "Image upload failed",
+            };
+        }
+
+        return {
+            ok: true,
+            message: res?.message || "Images uploaded successfully",
+            data: res.data,
+        };
+    } catch (error) {
+        return {
+            ok: false,
+            message: "Something went wrong",
+        };
+    }
+}
+
+export async function deleteImagesAction(payload: {
+    url: string | string[];
+}) {
+    try {
+        const res = await fileService.deleteImages(payload);
+
+        if (!res?.ok) {
+            return {
+                ok: false,
+                message: res?.message || "Image delete failed",
+            };
+        }
+
+        return {
+            ok: true,
+            message: res?.message || "Images deleted successfully",
+            data: res.data,
+        };
+    } catch (error) {
+        return {
+            ok: false,
+            message: "Something went wrong",
+        };
+    }
+}
