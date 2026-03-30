433867d99705bfa20d2a8ee4c62e5b9f01f50167
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-03-30 16:46:19 +0600

    fix: proxy redirect issue fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,40 +7,37 @@ export const dynamic = "force-dynamic"
 
 export async function proxy(request: NextRequest) {
      const pathname = request.nextUrl.pathname;
-     const data = await sessionService.getUserFromToken()
 
-     console.log("data",data);
+     const data = await sessionService.getUserFromToken();
 
+     const role = data?.role;
 
-     const role = data?.role
+     const isAdmin = role === Roles.admin;
+     const isUser = role === Roles.user;
 
-     const isAdmin = role === Roles.admin 
-     const isUser = role === Roles.user
-
-
- 
-     if (isAdmin && (pathname.startsWith("/dashboard"))) {
-          return NextResponse.redirect(new URL("/admin-dashboard", request.url))
+     // ✅ Admin trying to access user dashboard
+     if (isAdmin && pathname.startsWith("/dashboard")) {
+          return NextResponse.redirect(new URL("/admin-dashboard", request.url));
      }
 
-
-     if (isUser && (pathname.startsWith("/dashboard") || pathname.startsWith("/admin-dashboard"))) {
-          return NextResponse.redirect(new URL("/dashboard", request.url))
+     // ✅ User trying to access admin dashboard
+     if (isUser && pathname.startsWith("/admin-dashboard")) {
+          return NextResponse.redirect(new URL("/dashboard", request.url));
      }
 
-     
-
-     if(!role && (pathname.startsWith("/admin-dashboard") || pathname.startsWith("/user-dashboard"))){
-          return NextResponse.redirect(new URL("/", request.url))
+     // ✅ Unauthenticated user
+     if (
+          !role &&
+          (pathname.startsWith("/admin-dashboard") || pathname.startsWith("/dashboard"))
+     ) {
+          return NextResponse.redirect(new URL("/", request.url));
      }
 
-
-     return NextResponse.next()
+     return NextResponse.next();
 }
-
 export const config = {
      matcher: [
           "/admin-dashboard/:path*",
-          // "/dashboard/:path*",
+          "/dashboard/:path*",
      ],
 }
